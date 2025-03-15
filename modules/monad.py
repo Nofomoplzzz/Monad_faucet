bbb1d0894fe541dc6805a982b7cd9ed4c31ac19b
Nofomoplzzz <lazareff.david2017@yandex.ru>
2025-03-15 06:13:19 +0300

    Update monad.py

--- modules/monad.py
+++ modules/monad.py
@@ -2,7 +2,6 @@ import asyncio
 import random
 from loguru import logger
 from twocaptcha import TwoCaptcha
-import config
 from client import Client
 import aiohttp
 import utils
@@ -16,22 +15,29 @@ class Monad:
         self.version = self.user_agent.split('Chrome/')[1].split('.')[0]
 
         self.headers = {
-            'sec-ch-ua-platform': '"Windows"',
+            'accept': '*/*',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'no-cache',
+            'content-type': 'application/json',
+            'origin': 'https://testnet.monad.xyz',
+            'pragma': 'no-cache',
+            'priority': 'u=1, i',
             'referer': 'https://testnet.monad.xyz/',
-            'user-agent': self.user_agent,
             'sec-ch-ua': f'"Not(A:Brand";v="99", "Google Chrome";v="{self.version}", "Chromium";v="{self.version}"',
-            'Content-Type': 'application/json',
             'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
+            'user-agent': self.user_agent
         }
 
     async def faucet_mon(self):
         logger.info(
             f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Решаю капчу....')
 
-        result = TwoCaptcha(config.API_KEY)
-        captcha = result.turnstile(sitekey='0x4AAAAAAA-3X4Nd7hf3mNGx',
-                                   url='https://testnet.monad.xyz/'
-                                   )
+        result = TwoCaptcha('7f108abc2e46b1b25a0e9841dd7eee4c')
+        captcha = result.turnstile(sitekey='0x4AAAAAAA-3X4Nd7hf3mNGx', url='https://testnet.monad.xyz/')
 
         json_data = {
             'address': utils.get_account_address(private_key=self.client.private_key),
@@ -46,19 +52,53 @@ class Monad:
                         json=json_data,
                         proxy=self.client.proxy
                 ) as response:
-                    response_captcha = await response.json()
-                    logger.success(
-                        f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} {response_captcha}')
+                    response_captcha = await response.status
+                    if response_captcha == 200:
+                        logger.success(
+                            f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Drip successfully {response_captcha}')
+                    else:
+                        logger.warning(
+                            f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Not claimed {response_captcha}')
                     await asyncio.sleep(random.randint(1, 15))
         except aiohttp.ClientError as e:
             logger.error(
                 f'Profile: {self.client.profile} Ошибка клиента: {e}')
         except aiohttp.ClientOSError as e:
-            print(f"Профиль {self.client.profile} Ошибка соединения: {e}")
+            logger.error(
+                f'Profile: {self.client.profile} Ошибка соединения: {e}')
         except Exception as e:
-            print(f"Профиль {self.client.profile} Произошла ошибка: {e}")
+            logger.error(
+                f'Profile: {self.client.profile} Произошла ошибка: {e}')
 
-    async def balik(self):
+    async def send_transaction(self):
+        # balance = await client.w3.eth.get_balance(client.account.address)
+        # client.w3.from_wei(balance, 'ether')
+        count_value = random.uniform(0.001, 0.035)
+
+        logger.info(f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Отправляю транзакцию...')
+        tx = await self.client.send_transaction(
+            to=self.client.account.address,
+            from_=self.client.account.address,
+            value=self.client.w3.to_wei(count_value, 'ether'),
+            max_priority_fee_per_gas=self.client.max_priority_fee()
+        )
+
+
+        if tx:
+            try:
+                await self.client.verif_tx(tx_hash=tx)
+                logger.success(
+                    f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Transaction success!! tx_hash: {tx.hex()}')
+            except Exception as err:
+                logger.warning(
+                    f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Transaction error!! tx_hash: {tx.hex()}; error: {err}')
+                raise ValueError(f'{self.client.profile} Ошибка транзакции')
+        else:
+            logger.error(f'Profile: {self.client.profile} {utils.get_account_address(self.client.private_key)} Transaction error!!!')
+            raise ValueError(f'{self.client.profile} Ошибка транзакции')
+
+    async def   balik(self):
         balance = await self.client.w3.eth.get_balance(self.client.account.address)
         await self.client.w3.provider.disconnect()
+        # print(f"{self.profile} {self.client.w3.from_wei(balance, 'ether')}")
         print(self.client.profile, self.client.account.address, self.client.w3.from_wei(balance, 'ether'))
