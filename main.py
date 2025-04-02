a9b4fb6221bce398bc005ddc1ed9121bbe2e0708
Nofomoplzzz <lazareff.david2017@yandex.ru>
2025-04-02 14:49:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,8 @@ from modules.monad import Monad
 import csv
 
 
-async def process_row(private_key, proxy, semaphore, profile):
-    await semaphore.acquire()
-    monad = Monad(
-        Client(
-            private_key=private_key,
-            rpc='https://testnet-rpc.monad.xyz',
-            proxy=proxy,
-            profile=profile
-        )
-    )
-
-    await monad.faucet_mon()
-    # await monad.balik()
-    semaphore.release()
-
 
 async def main():
-    # Количество потоков
-    max_concurrent_tasks = 1
-    semaphore = asyncio.Semaphore(max_concurrent_tasks)
-
-    tasks = []
     with open('profiles.csv', mode='r') as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
@@ -33,10 +13,14 @@ async def main():
             proxy = row['Proxy']
             profile = row['Profile']
 
-            tasks.append(process_row(private_key, proxy, semaphore, profile))
+            monad = Monad(Client(
+                private_key= private_key,
+                rpc='https://testnet-rpc.monad.xyz',
+                proxy = proxy,
+                profile = profile
+            ))
 
-    await asyncio.gather(*tasks)
-
+            await monad.faucet_mon()
 
 if __name__ == "__main__":
     asyncio.run(main())
